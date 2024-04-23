06f7cc70e983eaa7cd994fe701b6d073a776b15c
onflou_hjh <hjh@onflou.com>
2024-04-23 19:57:11 +0900

    [Setting] 초기설정

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # springboot_blog
+
 Spring Boot 예제 프로젝트
---
+++ src/main/java/org/boot/blog/controller/TestController.java
@@ -0,0 +1,16 @@
+package org.boot.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TestController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public void hello(Model model) {
+
+        model.addAttribute("greeting", "Hello 타임리프.^^");
+    }
+}
